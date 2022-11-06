bbf1bbd4f6aedb5fd547a9a095ca3e1b915a1d42
dtinth on MBP M1 <dtinth@spacet.me>
2022-11-07 00:41:03 +0700

    Initial version
    Based on https://taskworld.github.io/prescript/guide/tips.html#retrying

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,26 @@
-export {}
+/**
+ * Retry a function until it succeeds or until a timeout is reached.
+ *
+ * @param f - The function to retry.
+ *  The function will receive the retry number as the first argument (starting at 0) and should return a promise.
+ * @param timeout - The timeout in milliseconds.
+ *  Note that the timeout will only be evaluated after the async function finishes.
+ *  If you want to abort the function after a certain time, you need to implement that logic in the function itself, e.g. by using the `p-timeout` npm package.
+ * @returns The result of the function.
+ */
+export async function retryUntilTimeout<T>(f: (retryNumber: number) => T, timeout = 15000) {
+  const start = Date.now()
+  let i = 0
+  for (;;) {
+    try {
+      return await f(i++)
+    } catch (error) {
+      if (Date.now() - start > timeout) {
+        throw error
+      } else {
+        const timeToWait = 100 + (Date.now() - start) / 10
+        await new Promise((r) => setTimeout(r, timeToWait))
+      }
+    }
+  }
+}
